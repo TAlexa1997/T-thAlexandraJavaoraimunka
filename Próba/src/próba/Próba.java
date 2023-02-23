a81f395677ad2112f926fe242c4fdbb094df51ff
T.Alexa <alejandra.marianella.8@gmail.com>
2023-02-23 17:43:05 +0100

    próba

--- Próba/src/próba/Próba.java
+++ Próba/src/próba/Próba.java
@@ -2,12 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+package próba;
 
 /**
  *
  * @author Amatory
  */
-public class Gyakorlás {
+public class Próba {
 
     /**
      * @param args the command line arguments
