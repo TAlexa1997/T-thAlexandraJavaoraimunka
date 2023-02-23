26e7e1643900b2f5df030ef17197ce1b41014367
Tóth Alexandra <118132673+TAlexa1997@users.noreply.github.com>
2023-02-23 17:54:24 +0100

    próba

--- TóthAlexandraóraimunka.java
+++ TóthAlexandraóraimunka.java
@@ -15,5 +15,13 @@ public class TóthAlexandraóraimunka {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+    public static void elso10SzamOsszege(){
+    int osszeg = 0;
+        int a = 1;
+        int b = 20;
+        for (int i = 0; i < 10; i++){
+            osszeg += i;
+        }
+        System.out.println("Az első 10 szám összege:" + osszeg "%d ! %d = %d\n", a, b, a+b);
+    }  
 }
